3a3c00d77b09aafcd308754f01d97e0f27b30718
olamiidee <osibeluwoolamide@gmail.com>
2022-10-20 02:15:00 +0100

    hehehe

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        green: "hsl(150, 100%, 66%)",
+        grayishBlue: "hsl(217, 19%, 38%)",
+        darkGrayishBlue: "hsl(217, 19%, 24%)",
+        darkBlue: "hsl(218, 23%, 16%)",
+      },
+    },
   },
   plugins: [],
-}
+};
